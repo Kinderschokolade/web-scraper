a3d8444f4e49c4752af21d7db93d1c45f952cdf8
Marius Bause <mariusbause@gmx.de>
2025-01-05 16:06:12 +0100

    towards Lambda

--- main.py
+++ main.py
@@ -4,22 +4,50 @@ from scrape import (
     clean_body_content,
     split_dom_content,
 )
-from parse import parse_with_llm 
+from parse import parse_with_local_llm, parse_with_cloud_llm
 
 
-if __name__=="__main__":
-    parse_description = "Wahlen 2025"
-    url = "https://www.sueddeutsche.de/"
-    #dom_content = scrape_website(url)
-    #body_content = extract_body_content(dom_content)
-    #cleaned_content = clean_body_content(body_content)
-    #with open("save.txt", "w") as text_file:
-    #    text_file.write(cleaned_content)
+def lambda_handler(event, context):
+    try:
+        # Get parameters from the event object
+        parse_description = event.get('parse_description', 'Wahlen 2025')
+        url = event.get('url', 'https://www.sueddeutsche.de/')
+        max_length = event.get('max_length', 500)
 
-    with open("save.txt", "r") as text_file:
-        cleaned_content =  text_file.read()
-    
-    dom_chunks = split_dom_content(cleaned_content,max_length=500)
-    
-    parsed_result = parse_with_llm(dom_chunks, parse_description)
-    print(parsed_result)
+        #scrape page
+        dom_content = scrape_website(url)
+        body_content = extract_body_content(dom_content)
+        cleaned_content = clean_body_content(body_content)
+        dom_chunks = split_dom_content(cleaned_content, max_length=max_length)
+        
+        parsed_result = parse_with_cloud_llm(dom_chunks, parse_description)
+
+        timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+        s3_filename = f"test/parsed_result_{timestamp}.json"
+        s3_client.put_object(
+            Bucket=web_parser,
+            Key=filename,
+            Body=json.dumps({
+                'parsed_result': parsed_result,
+            }),
+            ContentType='application/json'
+        )
+
+        # Return the results in the required Lambda response format
+        return {
+            'statusCode': 200,
+            'body': {
+                'parsed_result': parsed_result,
+                'url': url,
+                'parse_description': parse_description
+            }
+        }
+
+    except Exception as e:
+        # Error handling
+        return {
+            'statusCode': 500,
+            'body': {
+                'error': str(e)
+            }
+        }
--- parse.py
+++ parse.py
@@ -1,5 +1,7 @@
 from llama_cpp import Llama
 import pathlib
+from openai import OpenAI
+import os
 
 
 
@@ -7,18 +9,44 @@ template = (
     "You are tasked with summarizing information from the following news content: {dom_content}. "
 )
 
-llama_path=pathlib.Path(__file__).parents[1] / "llama-cpp/models/llama-3-70B/Meta-Llama-3-70B-Instruct-IQ2_XS.gguf"
-zephyr_path=pathlib.Path(__file__).parents[1] / "llama-cpp/models/zephyr-7b-beta.Q4_0.gguf"
 
-model = Llama(
-      model_path=str(zephyr_path),
-      n_gpu_layers=-1, # Uncomment to use GPU acceleration
-      # seed=1337, # Uncomment to set a specific seed
-      # n_ctx=2048, # Uncomment to increase the context window
+
+
+openai_client = OpenAI(
+    api_key = os.getenv("OPENAI_TOKEN"),
 )
 
 
-def parse_with_llama_local(dom_chunks, parse_description):
+def parse_with_cloud_llm(dom_chunks, parse_description):
+    parsed_results = []
+    for i, chunk in enumerate(dom_chunks, start=1):
+        print(f"Parsing batch: {i} of {len(dom_chunks)}")
+        prompt = template.format(dom_content=chunk)
+        response = openai_client.chat.completions.create(
+            model="gpt-4o-mini",
+            store=False,
+            messages=[
+                {"role": "user", "content": prompt}
+            ]
+        ).choices[0].message
+
+        print(f"Parsed batch: {response}")
+        parsed_results.append(response)
+
+    return "\n".join(parsed_results)   
+
+
+def parse_with_local_llm(dom_chunks, parse_description):
+    llama_path=pathlib.Path(__file__).parents[1] / "llama-cpp/models/llama-3-70B/Meta-Llama-3-70B-Instruct-IQ2_XS.gguf"
+    zephyr_path=pathlib.Path(__file__).parents[1] / "llama-cpp/models/zephyr-7b-beta.Q4_0.gguf"
+
+
+    model = Llama(
+        model_path=str(zephyr_path),
+        n_gpu_layers=-1, # Uncomment to use GPU acceleration
+        # seed=1337, # Uncomment to set a specific seed
+        # n_ctx=2048, # Uncomment to increase the context window
+    )
     parsed_results = []
     for i, chunk in enumerate(dom_chunks, start=1):
         print(f"Parsing batch: {i} of {len(dom_chunks)}")
